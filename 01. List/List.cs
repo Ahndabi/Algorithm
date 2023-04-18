fa608ea611de041147538cf886000654362411ec
안다비 안다비 <dksekqkr1234@naver.com>
2023-04-18 20:03:29 +0900

    feat : List 기능 구현

---
+++ 01. List/List.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure			// list 라고 하면 헷갈릴까봐 DataStructure 라고 바꿈
+{
+	internal class List<T>
+	{
+		private const int DefaultCapacity = 10;
+
+		private T[] items;       // 자료형 T 배열을 가지고 있다.
+		private int size;
+
+		public List()
+		{
+			this.items = new T[DefaultCapacity];
+			this.size = 0;
+		}
+
+		public int Capacity { get { return items.Length; } }
+
+		public int Count {  get { return size; } }
+
+		public T this[int index]		// 인덱서 구현 list[0] = 이런식으로 접근 가능하게 해줌
+		{
+			get
+			{
+				if (index < 0 || index >= size)                 
+					throw new IndexOutOfRangeException();       
+
+				return items[index];
+			}
+			set
+			{
+				if (index < 0 || index >= size)                 
+					throw new IndexOutOfRangeException();       
+
+				items[index] = value;
+			}
+		}
+
+		public void Add(T item)
+		{
+			if(size < items.Length)
+			{
+				items[size++] = item;
+			}
+			else
+			{
+				Grow();
+				items[size++] = item;
+			}
+		}
+
+		public bool Remove(T item)		// Remove는 반환형이 bool임!	제거했으면 true, 제거 못했으면 false
+		{
+			int index = IndexOf(item); 
+			if(index >= 0)
+			{   
+				// 찾았을 경우
+				RemoveAt(index);		// 그 위치에 있는 거 지워
+				return true;
+			}
+			else
+			{
+				// 못찾은 경우
+				return false;
+			}
+		}
+
+		public void RemoveAt(int index)		// index 번째 지워줘!
+		{
+			if (index < 0 || index >= size)					// 범위를 벗어나면
+				throw new IndexOutOfRangeException();		// 예외처리
+
+			size--;     // 지웠으니까 size 하나 줄어들지
+			Array.Copy(items, index + 1, items, index, size - index);
+			// items의 index+1부터, items의 index부터 size-index까지를 복사??
+		}
+
+
+		public int IndexOf(T item)		// 찾고자 하는 대상의 인덱스가 몇인지 구하는 함수
+		{
+			return Array.IndexOf(items, item, 0, size);		// 0부터 size까지 items에 item이 있는지 인덱스 추출
+		}
+
+		public T? Find(Predicate<T> match)
+		{
+			if (match == null)
+				throw new ArgumentNullException("match");       // match가 null이면 안돼! 예외처리
+
+			for (int i = 0; i < size; i++)
+			{
+				if (match(items[i]))
+					return items[i];
+			}
+
+			return default(T);
+		}
+
+		public int FindIndex(Predicate<T> match)
+		{
+			for(int i = 0; i <size; i++)
+			{
+				if (match(items[i]))
+					return i;
+			}
+			return - 1;
+		}
+		
+
+		public void Grow()		// 크기 키우기
+		{	
+			int newCapacity = items.Length * 2;
+			T[] newItems = new T[newCapacity];			// 2배 더 큰 배열을 만들었음.
+			Array.Copy(items, 0, newItems, 0, size);    // 새 배열에 원래 배열 데이터들 복사
+			items = newItems;							// items의 주소를 바꿈. newitems로.
+														// 그리고 newitems는 사라짐.
+														// 원래 배열도 가비지콜렉터에 의해 사라지게 될 거야
+		}
+	}
+}
---
+++ 01. List/Program.cs
@@ -0,0 +1,117 @@
+﻿// using System.Collections.Generic;	// 이거 없으면 list 사용 불가라고 함
+
+namespace _01._List
+{
+	internal class Program
+	{
+		/******************************************************
+		 * 배열 (Array)
+		 * 
+		 * 연속적인 메모리상에 동일한 타입의 요소를 일렬로 저장하는 자료구조
+		 * 초기화때 정한 크기가 소멸까지 유지됨
+		 * 배열의 요소는 인덱스를 사용하여 직접적으로 엑세스 가능
+		 * 자료의 개수가 정해져 있을 때 사용
+		 ******************************************************/
+
+		// <배열의 사용>
+		void Array()
+		{
+			int[] intArray = new int[100];
+
+			// 인덱스를 통한 접근
+			intArray[0] = 10;
+			int value = intArray[0];
+		}
+
+		// <배열의 시간복잡도>
+		// 접근		탐색
+		// O(1)		O(N)
+
+		// int 배열 20번째 자료 접근 : 20번째 자료의 주소 = 배열의 주소 + int 자료형의 크기 * 19
+		// 데이터가 n개 있을 때 탐색 (ex. 10이라는 숫자는 배열의 몇번째 순서에 있을까?) (최악의 경우 n번 진행)
+		/*
+		public int FindIndex(int[] intArray, int data)
+		{
+			for(int i = 0; i < intArray.Length; i++)
+			{
+				if (intArray[i] == data)
+				{
+					return i;
+				}
+			}
+			return -1;
+		} */        // 배열 탐색은 선형탐색이어야 함
+
+
+
+
+		/******************************************************
+		 * 리스트 (동적배열) (Dynamic Array)
+		 * 
+		 * 런타임 중 크기를 확장할 수 있는 배열기반의 자료구조
+		 * 배열요소의 갯수를 특정할 수 없는 경우 사용
+		 * 크기가 안 정해져 있는 배열
+		 * 리스트는 클래스임
+		 ******************************************************/
+
+		// 미리 크게 배열 양을 잡아놓고 거기 안에서 추가 추가 추가...
+
+		// <List의 사용>
+		void List()
+		{
+			List<string> list = new List<string>();		// <담고싶은 자료형> 
+														// 리스트는 클래스라서 <>일케 사용
+
+			// 배열 요소 삽입
+			list.Add("0번 데이터");
+			list.Add("1번 데이터");
+			list.Add("2번 데이터");
+
+			// 배열 요소 삭제
+			list.Remove("1번 데이터");
+
+			// 배열 요소 접근
+			list[0] = "데이터0";
+			string value = list[0];
+
+			// 배열 요소 탐색
+			string? findValue = list.Find(x => x.Contains('2'));
+			int findIndex = list.FindIndex(x => x.Contains('0'));
+		}
+
+		// <List의 시간복잡도>
+		// 접근		탐색	삽입	삭제
+		// O(1)		O(n)	O(n)	O(n)
+
+	
+		static void Main(string[] args)
+		{
+			List<string> list = new List<string>();
+
+			list.Add("1번 데이터");
+			list.Add("2번 데이터");
+			list.Add("3번 데이터");
+			list.Add("4번 데이터");
+			list.Add("5번 데이터");
+
+			string value;
+			value = list[0];
+			value = list[1];
+			value = list[2];
+			value = list[3];
+			value = list[4];
+
+			list[0] = "5번 데이터";
+			list[1] = "4번 데이터";
+			list[2] = "3번 데이터";
+			list[3] = "2번 데이터";
+			list[4] = "1번 데이터";
+
+			list.Remove("3번 데이터");
+			list.Remove("2번 데이터");
+
+			string? findValue = list.Find(x => x.Contains('4'));
+			int findIndex = list.FindIndex(x => x.Contains('1'));
+		}
+	}
+}

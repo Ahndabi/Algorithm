27a81e60f782293c5100045b9aa6fb15a45ea25a
안다비 안다비 <dksekqkr1234@naver.com>
2023-04-21 21:33:41 +0900

    feat : Stack

---
+++ 04.Stack/Stack.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure		// 네임스페이스 DataStructure 로 바꿈
+{
+	internal class Stack
+	{
+		// 
+	}
+}

5bfc4b21f379182731f017af2d832115e8a723a5
안다비 안다비 <dksekqkr1234@naver.com>
2023-04-27 16:34:38 +0900

    feat : BinarySearchTree 구현

---
+++ 07.BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+// 이것도 뭔가 이상,, 깃허브 봐(그냥 내가 이상하게 한듯)
+
+namespace DataStructure		// 네임스페이스 구현
+{
+	// 이진탐색트리 구현
+	internal class BinarySearchTree<T> where T : IComparable<T>	// T item 데이터가 비교가 가능해야 함.
+	{
+		Node root;		// 최상위 노드
+
+		public BinarySearchTree()
+		{
+			this.root = null;
+		}
+
+		public void Add(T item)
+		{
+			Node newNode = new Node(item, null, null, null);
+
+			if(root == null)
+			{
+				root = newNode;
+				return;
+			}
+
+			Node current = root;
+			while(current != null)
+			{
+				// 비교해서 더 작은 경우 왼쪽으로 감
+				if(item.CompareTo(current.item) < 0)
+				{
+					// 비교 노드가 왼쪽 자식이 있는 경우
+					if(current.left != null)
+					{
+						// current는 왼쪽이랑 또 비교해야 함(이걸 반복)
+						// 왼쪽 자식과 또 비교하기 위해 current 왼쪽 자식으로 설정
+						current = current.left;	
+					}
+					// 비교 노드가 왼쪽 자식이 없는 경우
+					else
+					{
+						// 그자리가 지금 추가가 될 자리
+						current.left = newNode;
+						newNode.parent = current;
+						return;
+					}
+				}
+				// 더 큰 경우에는 오른쪽으로 감
+				else if (item.CompareTo(current.item) > 0)
+				{
+					// 비교 노드가 오른쪽 자식이 있는 경우
+					if (current.right != null)
+					{
+						//오른쪽 자식과 또 비교하기 위해 current 오른쪽 자식으로 설정
+						current = current.right;
+					}
+					// 비교 노드가 오른쪽 자식이 없는 경우
+					else
+					{
+						// 그 자리가 지금 추가가 될 자리
+						current.right = newNode;
+						newNode.parent = current;
+						return;
+					}
+
+				}
+				// 동일한 데이터가 들어온 경우
+				else
+				{
+					return;
+				}
+
+			}
+		}
+
+		// out T outValue : 출력전용 파라메타. 여기 넣은 변수도 출력대상임
+		// 함수 종료 전까지 outValu에 꼭 넣어라?
+		public bool TryGetValue(T item, out T outValue)		
+		{
+			Node findNode = FindNode(item);
+			if (findNode == null)
+			{
+				outValue = default(T);
+				return false;
+			}
+			else
+			{
+				outValue = findNode.item;
+				return true;
+			}
+		}
+
+		public bool Remove(T item)
+		{
+			Node findNode = FindNode(item);
+			if(findNode == null)
+			{
+				return false;
+			}
+			else
+			{
+				EraseNode(findNode);
+				return true;
+			}
+		}
+
+		private Node FindNode(T item)
+		{
+			if (root == null)
+				return null;
+
+			Node current = root;
+			while (current != null)
+			{
+				// 현재 노드의 값이 찾고자 하는 값보다 작은 경우
+				if (item.CompareTo(current.item) < 0)
+				{
+					// 왼쪽 자식부터 다시 찾기 시작
+					current = current.left;
+				}
+
+				// 현재 노드의 값이 찾고자 하는 값보다 큰 경우
+				else if (item.CompareTo(current.item) > 0)
+				{
+					// 오른쪽 자식부터 다시 찾기 시작
+					current = current.right;
+				}
+
+				// 현재 노드의 값이 찾고자 하는 값이랑 같은 경우
+				else
+				{
+					// 찾음
+					return current;		// 그 노드 자체를 반환
+				}
+			}
+			return null;
+		}
+
+		void EraseNode(Node node)
+		{
+			// 자식 노드가 없는 노드일 경우
+			if(node.HasNoChild)
+			{
+				if (node.IsLeftChild)
+					node.parent.left = null;
+				else if (node.IsRightChild)
+					node.parent.right = null;
+				else  // if(node.IsRootNode) 부모노드라면
+					root = null;
+			}
+			// 자식 노드가 1개인 노드일 경우
+			else if(node.HasLeftChild || node.HasRightChild)
+			{
+				Node parent = node.parent;
+				Node child = node.HasLeftChild ? node.left : node.right;
+				
+				if(node.IsLeftChild)
+				{
+					parent.left = child;
+					child.parent = parent;
+				}
+				else if(node.IsRightChild)
+				{
+					parent.right = child;
+					child.parent = parent;
+				}
+				else  // if(node.IsRootNode) 내가 최정상 노드라면
+				{
+					root = child;
+					child.parent = null;
+				}
+			}
+			// 3. 자식 노드가 2개인 노드일 경우
+			// 왼쪽 자식 중 가장 큰값과 데이터 교환한 후, 그 노드를 지워주는 방식으로 대체
+			else   // if(node.HasBothChild)
+			{
+				Node replaceNode = node.left;
+				while(replaceNode.right != null)
+				{
+					replaceNode = replaceNode.right;
+				}
+				node.item = replaceNode.item;
+				EraseNode(replaceNode);
+			}
+		}
+
+		/*
+		public void Print(Node node)     // 중위순회
+		{
+			if(node.HasLeftChild)  Print(node.left);
+			Console.WriteLine(node.item);
+			if (node.HasRightChild) Print(node.right);
+		}*/	// 왜 오류나징..
+
+		class Node
+		{
+			internal T item;	
+			internal Node parent;
+			internal Node left;
+			internal Node right;
+
+			public Node(T item, Node parent, Node left, Node right)
+			{
+				this.item = item;
+				this.parent = parent;
+				this.left = left;
+				this.right = right;
+			}
+
+			public bool IsRootNode { get { return parent == null; } }	// 내가 부모노드
+			public bool IsLeftChild { get { return parent != null && parent.left == this; } }	// 내가 왼쪽자식
+			public bool IsRightChild { get { return parent != null && parent.right == this; } }	 // 내가 오른쪽자식
+
+			public bool HasNoChild { get { return left == null && right == null; } } // 자식X
+			public bool HasLeftChild { get { return left != null && right == null; } } // 오자식X
+
+			public bool HasRightChild { get { return left == null && right != null; } } // 왼자식X
+			public bool HasBothChild { get { return left != null && right != null; } } // 자식O
+		}
+
+	}
+}
---
+++ 07.BinarySearchTree/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+
+namespace _07.BinarySearchTree
+{
+	internal class Program
+	{
+		/******************************************************
+		 * 트리 (Tree)
+		 * 
+		 * 계층적인 자료를 나타내는데 자주 사용되는 자료구조
+		 * 부모노드가 0개 이상의 자식노드들을 가질 수 있음
+		 * 한 노드에서 출발하여 다시 자기 자신의 노드로 돌아오는 순환구조를 가질 수 없음
+		 ******************************************************/
+
+		/******************************************************
+		 * 이진탐색트리 (BinarySearchTree)
+		 * 
+		 * 이진속성과 탐색속성을 적용한 트리
+		 * 이진탐색을 통한 탐색영역을 절반으로 줄여가며 탐색 가능
+		 * 이진 : 부모노드는 최대 2개의 자식노드들을 가질 수 있음
+		 * 탐색 : 자신의 노드보다 작은 값들은 왼쪽, 큰 값들은 오른쪽에 위치
+		 * 완전이진트리를 보장하지 않기 때문에 배열기반으로 짤 수가 없음. 노드로 짜야함. 
+		 * 그래서 C#에서 그렇게 선호하진 않음,,
+		 * 들어오는 순서에 따라서 모양이 달라짐.
+		 * 노드기반의 비선형구조임.
+		 ******************************************************/
+		// heap은 완전이진트리를 보장하기에 배열로 짤 수 있음.
+
+		// heap은 부모노드가 무조건 자식노드보다 우선순위이고,
+		// 이진탐색트리는 부모노드가 왼쪽자식이랑 오른쪽 자식의 중간값임
+		// 대규모 저장소에서 주로 사용됨. 거기서 내가 찾고자하는 데이터를 찾기.
+		// 정렬이 보장되어 있는 자료구조 중 하나임.
+
+
+		// <이진탐색트리의 시간복잡도>
+		// 접근			탐색	    삽입		삭제
+		// O(log n)		O(log n)	O(log n)	O(log n)
+
+		// <이진탐색트리의 주의점>	//****** 기술면접 나올 수 있음
+		// 이진탐색트리의 노드들이 한쪽 자식으로만 추가되는 불균형 발생 가능(ex. 계속해서 큰값만 추가되는경우?)
+		// 이 경우 탐색영역이 절반으로 줄여지지 않기 때문에 시간복잡도 증가
+		// 이러한 현상을 막기 위해 자가균형기능을 추가한 트리의 사용이 일반적
+		// 대표적인 방식으로 Red-Black Tree, AVL Tree 등이 있음
+		// 노드의 균형을 맞추기 
+
+		// <트리기반 자료구조 순회>
+		// 1. 전위순회 : 노드, 왼쪽, 오른쪽
+		// 2. 중위순회 : 왼쪽, 노드, 오른쪽		<- 이진탐색트리의 순회 (오름차순으로 정렬되기 때문)
+		// 3. 후위순회 : 왼쪽, 오른쪽, 노드
+		
+		static void BinarySearchTree()
+		{
+			// 키 == 데이터
+			SortedSet<int> sortedSet = new SortedSet<int>();
+
+			// 추가
+			sortedSet.Add(1);
+			sortedSet.Add(2);
+			sortedSet.Add(3);
+			sortedSet.Add(4);
+			sortedSet.Add(5);
+
+			//탐색
+			int searchValue;
+			bool find = sortedSet.TryGetValue(3, out searchValue);  // 없을 수도 있어서 bool을 통해 탐색시도
+
+			// 삭제
+			sortedSet.Remove(3);
+
+			// 탐색용 키(int), 실제 데이터(string)
+			SortedDictionary<string, Monster> sortedDic = new SortedDictionary<string, Monster>();
+
+			sortedDic.Add("피카츄", new Monster() { name = "피카츄", hp = 100 });
+			sortedDic.Add("파이리", new Monster() { name = "파이리", hp = 100 });
+			sortedDic.Add("꼬부기", new Monster() { name = "꼬부기", hp = 100 });
+			sortedDic.Add("리아코", new Monster() { name = "리아코", hp = 100 });
+			sortedDic.Add("이상해씨", new Monster() { name = "이상해씨", hp = 100 });
+
+			Monster monster;
+			sortedDic.TryGetValue("파이리", out monster);   // 파이리 탐색 시도
+			Monster indexerMonster = sortedDic["파이리"];  // 인덱서를 통한 탐색
+			// 인덱서에 키 값을 넣음(sortedDic 자료구조에서 "파이리"를 찾아줘~
+
+			sortedDic.Remove("리아코");
+		}
+
+		class Monster
+		{
+			public string name;
+			public int hp;
+			public int mp;
+			public int ap;
+			public int dp;
+
+		}
+
+		static void Main(string[] args)
+		{
+			BinarySearchTree();
+		}
+
+	}
+}

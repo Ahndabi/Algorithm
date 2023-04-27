de8a2ab008e65b595cc78fa4666109736a8b656a
안다비 안다비 <dksekqkr1234@naver.com>
2023-04-27 21:48:50 +0900

    feat : 알고리즘 실습

---
+++ 실습/NM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NM
+{
+	internal class NM	// 완성을 못했습니다.......
+	{                           // 4     2
+		static void GetArray(int N, int M)  // (1 <= M <= N <= 7)
+		{
+			for (int i = 1; i <= N; i++)
+			{
+				for (int j = 1; j <= N; j++)        // 줄바꿈
+				{
+					for (int k = 1; k < 2; k++)
+					{
+						Console.Write(i);
+						Console.Write(" ");
+						Console.Write(j);
+					}
+					Console.WriteLine();
+				}
+			}
+		}
+
+		static void Main()
+		{
+			GetArray(4, 2);
+		}
+	}
+}
--- 실습/하노이탑.cs
+++ 실습/하노이탑.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections.Specialized;
-using System.Diagnostics;
-using System.Drawing;
-using System.Runtime.CompilerServices;
-using System.Xml.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace 실습
 {
-	internal class Program
-	{ 
+	internal class 하노이탑		// 하다가 포기했습니다..
+	{
 		static Stack<int>[] stick = new Stack<int>[3];         // 스택 형태의 stick(기둥)을 만든다. 크기는 3칸. 0, 1, 2 기둥이 있다.
 
 		static void Move2(int count, int start, int end)
@@ -20,9 +20,9 @@ namespace 실습
 			if (count == 1)
 			{
 				int circle = stick[start].Pop();    // 첫번째 기둥에서 마지막 남은 원반을 뺀다.
-				stick[end].Push(circle);		// 마지막 기둥에(2번째) 그 원반을 넣는다.
+				stick[end].Push(circle);        // 마지막 기둥에(2번째) 그 원반을 넣는다.
 				Console.WriteLine($"{circle} {stick}");
-				Move2(count - count, start, end) ;
+				Move2(count - count, start, end);
 
 			}
 
@@ -30,18 +30,18 @@ namespace 실습
 			Move2(1, start, end);
 			Move2(count - 1, other, end);
 			// Move2(count - 2, start, other);
-			
+
 		}
 
-		static void Main2()
+		static void Main1()
 		{
 
 			for (int i = 0; i < 3; i++)
 			{
-				stick[i] = new Stack<int>();	// 각 스택 0, 1, 2 기둥을 만든다.
+				stick[i] = new Stack<int>();    // 각 스택 0, 1, 2 기둥을 만든다.
 			}
 
-			for(int i = 0; i < 4; i++)	// 원반을 스택에다가 넣음
+			for (int i = 0; i < 4; i++) // 원반을 스택에다가 넣음
 			{
 				stick[0].Push(i);
 			}
